ad70305b57bffccb08f42f0c4cee61773a780ef9
jeesssik <jeesssik@gmail.com>
2026-04-20 14:16:03 -0300

    Add MenuManager script and associated meta files for main menu functionality
    - Created MenuManager.cs to handle main menu interactions including start, options, credits, and exit buttons.
    - Added meta file for MenuManager.cs for Unity asset management.
    - Introduced scripts folder meta file to organize script assets.

---
+++ Assets/scripts/MenuManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Canvases")]
+    public GameObject mainMenuCanvas;
+    public GameObject optionsCanvas;
+    public GameObject creditsCanvas;
+
+    [Header("Scene")]
+    public string gameSceneName = "GameScene";
+
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    // ------------------------
+    // BOTONES PRINCIPALES
+    // ------------------------
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OnOptionsButton()
+    {
+        mainMenuCanvas.SetActive(false);
+        optionsCanvas.SetActive(true);
+        creditsCanvas.SetActive(false);
+    }
+
+    public void OnCreditsButton()
+    {
+        mainMenuCanvas.SetActive(false);
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(true);
+    }
+
+    public void OnExitButton()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+
+    // ------------------------
+    // VOLVER AL MENÚ
+    // ------------------------
+
+    public void OnBackButton()
+    {
+        ShowMainMenu();
+    }
+
+    private void ShowMainMenu()
+    {
+        mainMenuCanvas.SetActive(true);
+        optionsCanvas.SetActive(false);
+        creditsCanvas.SetActive(false);
+    }
+}
